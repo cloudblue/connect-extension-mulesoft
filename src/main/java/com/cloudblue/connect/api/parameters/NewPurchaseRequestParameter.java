b126d1dbe6008b96f8001fce1ad4f47eb64dbff5
Martin Constante <martin.constante@ingrammicro.com>
2021-06-21 13:08:56 +0200

    LITE-18854 Modify Create Purchase Request Parameters

--- src/main/java/com/cloudblue/connect/api/parameters/NewPurchaseRequestParameter.java
+++ src/main/java/com/cloudblue/connect/api/parameters/NewPurchaseRequestParameter.java
@@ -1,6 +1,5 @@
 package com.cloudblue.connect.api.parameters;
 
-import com.cloudblue.connect.api.models.*;
 import com.cloudblue.connect.api.models.enums.CBCRequestType;
 
 import java.util.HashMap;
@@ -15,6 +14,7 @@ import com.cloudblue.connect.api.models.subscription.CBCRequest;
 import com.cloudblue.connect.api.models.subscription.CBCRequestItem;
 import com.cloudblue.connect.api.models.subscription.CBCRequestParam;
 import com.cloudblue.connect.api.models.tier.CBCAccount;
+import com.cloudblue.connect.api.parameters.accounts.AccountParameter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
@@ -38,15 +38,15 @@ public class NewPurchaseRequestParameter implements Embeddable {
     private String externalUid = UUID.randomUUID().toString();
     
     @Parameter
-    private CBCAccount customer;
+    private AccountParameter customer;
     
     @Parameter
     @Optional
-    private CBCAccount tier1;
+    private AccountParameter tier1;
     
     @Parameter
     @Optional
-    private CBCAccount tier2;
+    private AccountParameter tier2;
     
     @Parameter
     private List<CBCRequestItem> items;
@@ -97,27 +97,27 @@ public class NewPurchaseRequestParameter implements Embeddable {
         this.externalUid = externalUid;
     }
 
-    public CBCAccount getCustomer() {
+    public AccountParameter getCustomer() {
         return customer;
     }
 
-    public void setCustomer(CBCAccount customer) {
+    public void setCustomer(AccountParameter customer) {
         this.customer = customer;
     }
 
-    public CBCAccount getTier1() {
+    public AccountParameter getTier1() {
         return tier1;
     }
 
-    public void setTier1(CBCAccount tier1) {
+    public void setTier1(AccountParameter tier1) {
         this.tier1 = tier1;
     }
 
-    public CBCAccount getTier2() {
+    public AccountParameter getTier2() {
         return tier2;
     }
 
-    public void setTier2(CBCAccount tier2) {
+    public void setTier2(AccountParameter tier2) {
         this.tier2 = tier2;
     }
 
@@ -159,10 +159,30 @@ public class NewPurchaseRequestParameter implements Embeddable {
             this.getCustomer().setExternalUid(UUID.randomUUID().toString());
 
         request.getAsset().setTiers(new HashMap<>());
-        request.getAsset().getTiers().put("customer", this.getCustomer());
-        request.getAsset().getTiers().put("tier1", this.getTier1());
-        request.getAsset().getTiers().put("tier2", this.getTier2());
 
+        CBCAccount account = new CBCAccount();
+        account.setCompanyName(this.getCustomer().getCompanyName());
+        account.setExternalId(this.getCustomer().getExternalId());
+        account.setExternalUid(this.getCustomer().getExternalUid());
+        account.setContactInfo(this.getCustomer().getContactInfo());
+        request.getAsset().getTiers().put("customer", account);
+
+        if (this.getTier1() != null) {
+
+            account.setCompanyName(this.getTier1().getCompanyName());
+            account.setExternalId(this.getTier1().getExternalId());
+            account.setExternalUid(this.getTier1().getExternalUid());
+            account.setContactInfo(this.getTier1().getContactInfo());
+            request.getAsset().getTiers().put("tier1", account);
+        }
+
+        if (this.getTier2() != null) {
+            account.setCompanyName(this.getTier2().getCompanyName());
+            account.setExternalId(this.getTier2().getExternalId());
+            account.setExternalUid(this.getTier2().getExternalUid());
+            account.setContactInfo(this.getTier2().getContactInfo());
+            request.getAsset().getTiers().put("tier2", account);
+        }
         request.getAsset().setConnection(new CBCConnection());
         request.getAsset().getConnection().setId(this.getConnectionId());
 
